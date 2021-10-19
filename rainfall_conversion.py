e0c7ba061e16e322022ee69dbfe3c62c2b52f1af
Secov8 <svc1u18@soton.ac.uk>
2021-10-19 13:38:06 +0200

    Added m placeholder

--- rainfall_conversion.py
+++ rainfall_conversion.py
@@ -1,1 +1,12 @@
-""" Al library"""
+"""A library to perform rainfall unit conversions"""
+
+def inches_to_mm(inches):
+    """Convert inches to milimetres.
+
+    Arguments:
+    inches -- the rainfall inches
+    """
+    mm = inches * 25.4
+    return mm
+
+# TODO: Add function inches_to_m"
